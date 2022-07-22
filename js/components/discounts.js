6e30e65c96fe2f2b3763be5625a557abfc3b3480
IT-Academy-Gomel <IT-Academy-Gomel>
2022-07-22 21:39:13 +0300

    add products

--- js/components/discounts.js
+++ js/components/discounts.js
@@ -1,19 +1,37 @@
-const swiper = new Swiper('.discount__swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 3,
-    spaceBetween: 20,
-  
-    // If we need pagination
-    pagination: {
+new Swiper('.discounts__swiper', {
+  // Optional parameters
+  loop: true,
+  direction: 'horizontal',
+  slidesPerView: 3,
+  spaceBetween: 20,
+
+  breakpoints: {
+      // when window width is >= 320px
+      320: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+      },
+      // when window width is >= 480px
+      480: {
+          slidesPerView: 3,
+          spaceBetween: 30
+      },
+      // when window width is >= 640px
+      640: {
+          slidesPerView: 3,
+          spaceBetween: 40
+      }
+  },
+
+  // If we need pagination
+  pagination: {
       el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
+  },
+
+  // Navigation arrows
+  navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    },  
-   
-  });
+  },
+
+});
